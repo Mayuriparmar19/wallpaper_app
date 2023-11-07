07d6cecb2bbd3dcc76dbf83264b0f46e0c4b2c77
System 16 <mittuparmar19@gmail.com>
2023-11-07 13:10:29 +0530

    all done

--- lib/fullscreen.dart
+++ lib/fullscreen.dart
@@ -3,36 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 class FullScreenData extends StatefulWidget {
-   final String imageUrl;
+  final String imageUrl;
 
-   const FullScreenData(this.imageUrl, {super.key});
-
-
+  const FullScreenData(this.imageUrl, {super.key});
 
   @override
   State<FullScreenData> createState() => _FullScreenDataState();
 }
 
 class _FullScreenDataState extends State<FullScreenData> {
-
-
-  Future <void>setLockWallpaper ()async
-  {
-              int location = WallpaperManager.LOCK_SCREEN;
-              var file = await DefaultCacheManager().getSingleFile(widget.imageUrl);
-              var res = await WallpaperManager.setWallpaperFromFile(file.path, location);
-              // String res= (await WallpaperManager.setWallpaperFromFile(file.path, location)) as String;
+  Future<void> setLockWallpaper() async {
+    int location = WallpaperManager.LOCK_SCREEN;
+    var file = await DefaultCacheManager().getSingleFile(widget.imageUrl);
+    var res = await WallpaperManager.setWallpaperFromFile(file.path, location);
+    // String res= (await WallpaperManager.setWallpaperFromFile(file.path, location)) as String;
   }
-  Future <void>setHomeWallpaper ()async
-  {
+
+  Future<void> setHomeWallpaper() async {
     int location = WallpaperManager.HOME_SCREEN;
     var file = await DefaultCacheManager().getSingleFile(widget.imageUrl);
     var res = await WallpaperManager.setWallpaperFromFile(file.path, location);
     // String res= (await WallpaperManager.setWallpaperFromFile(file.path, location)) as String;
   }
-  Future <void>setBothWallpaper ()async
-  {
+
+  Future<void> setBothWallpaper() async {
     int location = WallpaperManager.BOTH_SCREEN;
     var file = await DefaultCacheManager().getSingleFile(widget.imageUrl);
     var res = await WallpaperManager.setWallpaperFromFile(file.path, location);
@@ -49,16 +45,15 @@ class _FullScreenDataState extends State<FullScreenData> {
         leading: InkWell(
             onTap: () => Navigator.pop(context),
             child: const Icon(CupertinoIcons.back)),
-
       ),
       body: Container(
         color: Colors.black,
         child: Column(
           children: [
-            Expanded(child: Container(
+            Expanded(
+                child: Container(
               color: Colors.transparent,
-
-                child: Image.network(widget.imageUrl),
+              child: Image.network(widget.imageUrl),
             )),
             Container(
               height: 60,
@@ -69,18 +64,17 @@ class _FullScreenDataState extends State<FullScreenData> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   InkWell(
-                    onTap: (){
+                    onTap: () {
                       Fluttertoast.showToast(
-
                           msg: "Lock Screen WallPaper is Set",
                           toastLength: Toast.LENGTH_SHORT,
                           gravity: ToastGravity.BOTTOM,
                           timeInSecForIosWeb: 1,
                           backgroundColor: Colors.black,
                           textColor: Colors.white,
-                          fontSize: 16.0
-                      );
-                      setLockWallpaper();},
+                          fontSize: 16.0);
+                      setLockWallpaper();
+                    },
                     child: const Text(
                       'Lock Screen ',
                       style: TextStyle(fontSize: 16, color: Colors.white),
@@ -89,19 +83,16 @@ class _FullScreenDataState extends State<FullScreenData> {
                   InkWell(
                     onTap: () {
                       Fluttertoast.showToast(
-
-                          msg: "home Screen WallPaper is Set",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.black,
-                          textColor: Colors.white,
-                          fontSize: 16.0,
-
+                        msg: "home Screen WallPaper is Set",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.black,
+                        textColor: Colors.white,
+                        fontSize: 16.0,
                       );
                       setHomeWallpaper();
                     },
-
                     child: const Text(
                       'Home Screen ',
                       style: TextStyle(fontSize: 16, color: Colors.white),
@@ -110,15 +101,13 @@ class _FullScreenDataState extends State<FullScreenData> {
                   InkWell(
                     onTap: () {
                       Fluttertoast.showToast(
-
                           msg: " On Both Screen WallPaper is Set",
                           toastLength: Toast.LENGTH_SHORT,
                           gravity: ToastGravity.BOTTOM,
                           timeInSecForIosWeb: 1,
                           backgroundColor: Colors.black,
                           textColor: Colors.white,
-                          fontSize: 16.0
-                      );
+                          fontSize: 16.0);
                       setBothWallpaper();
                     },
                     child: const Text(
